e9fdcb974548b588f97f5870b68b1707cd2cbb3e
brunomalka <developer.brunoazevedo@gmail.com>
2019-07-24 00:01:27 -0300

    .

--- app.js
+++ app.js
@@ -29,8 +29,15 @@
         app.engine('handlebars', handlebars({defaultLayout:'main'}))
         app.set('view engine', 'handlebars')
     // Mongoose
+        //provide a sensible default for local development
+        const db_name = "blogapp";
+        mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + db_name;
+        //take advantage of openshift env vars when available:
+        if(process.env.OPENSHIFT_MONGODB_DB_URL){
+            mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
+        }
         mongoose.Promise = global.Promise;
-        mongoose.connect('mongodb://localhost/blogapp').then(()=>{
+        mongoose.connect(mongodb_connection_string).then(()=>{
             console.log('conectado ao mongo;')
         }).catch((err)=>{
             console.log('erro ao se conectar : '+err)
@@ -41,7 +48,11 @@
 // Rotas
     app.use('/admin', admin)
 // Outros
-const PORT = 8080
-app.listen(PORT, () =>  {
-    console.log("Servidor Rodando na porta "+PORT)
+
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+app.listen(server_port, server_ip_address, () =>  {
+    console.log("Servidor Rodando "+server_ip_address+":"+server_port)
 })
